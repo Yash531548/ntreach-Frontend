07652dfe220079fa196ca2a28c03506a548fd896
Yash531548 <yashsharma531548@gmail.com>
2025-09-01 15:40:58 +0530

    Update ResultData Component

--- src/components/Dashboard/ResultData.jsx
+++ src/components/Dashboard/ResultData.jsx
@@ -106,7 +106,7 @@ const ResultData = ({ setSubView, setSelectedView }) => {
                                         </span>
                                     )}
                                     {report.reportType === "reupload" && (
-                                        <span className="text-red-500 cursor-pointer hover:underline flex items-center gap-1 ml-[-12px] ">
+                                        <span className="text-red-500 cursor-pointer hover:underline flex items-center gap-1 ml-[0px] ">
                                             <span className="h-2 w-2 rounded-full bg-red-500  "></span>
                                             {report.report}
                                         </span>
