bc09996f55cf62eeb6476ac746b30f4c3ce4e93b
Yash531548 <yashsharma531548@gmail.com>
2025-09-02 13:40:41 +0530

    Update Rotating Words

--- src/components/RotatingWords.jsx
+++ src/components/RotatingWords.jsx
@@ -1,24 +1,3 @@
-// import React from "react";
-// import "./RotatingText.css";
-
-// export default function RotatingText() {
-//     const words = ["Consultation", "PrEP Consultation", "STI Test", "HIV Test"];
-
-//     return (
-//         <h1 className="text-4xl font-semibold flex gap-2 overflow-hidden  w-full whitespace-nowrap" style={{ fontFamily: 'Sofia Pro', fontWeight: 400 }}>
-//             Book a free 
-//             <span className="relative h-8 ">
-//                 <span className="absolute flex flex-col rotating-words    ">
-//                     {words.map((word, i) => (
-//                         <span key={i} className="text-[#1475A1] ">
-//                             {word}
-//                         </span>
-//                     ))}
-//                 </span>
-//             </span>
-//         </h1>
-//     );
-// }
 import React from "react";
 import "./RotatingText.css";
 
