ffa563f7f4b3004ca5ec662c1e5e305e46381741
mynkbhurani <154728413+mynkbhurani@users.noreply.github.com>
2025-10-06 15:40:17 +0530

    Update LanguageSelector.jsx

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -3,32 +3,33 @@ import { ChevronDown } from 'lucide-react' // icons
 
 const languages = [
   { code: 'en', label: 'English', short: 'EN' },
+  { code: 'as', label: 'Assamese', short: 'AS' },
+  { code: 'bn', label: 'Bengali', short: 'BN' },
+  { code: 'brx', label: 'Bodo', short: 'BRX' },
+  { code: 'bho', label: 'Bhojpuri', short: 'BHO' },
+  { code: 'doi', label: 'Dogri', short: 'DOI' },
+  { code: 'gu', label: 'Gujarati', short: 'GU' },
   { code: 'hi', label: 'Hindi', short: 'HI' },
-  { code: 'fr', label: 'French', short: 'FR' },
-  { code: 'es', label: 'Spanish', short: 'ES' }
+  { code: 'kn', label: 'Kannada', short: 'KN' },
+  { code: 'kha', label: 'Khasi', short: 'KHA' },
+  { code: 'kok', label: 'Konkani', short: 'KOK' },
+  { code: 'trp', label: 'Kokborok', short: 'TRP' },
+  { code: 'mai', label: 'Maithili', short: 'MAI' },
+  { code: 'ml', label: 'Malayalam', short: 'ML' },
+  { code: 'mni', label: 'Manipuri (Meiteilon)', short: 'MNI' },
+  { code: 'mr', label: 'Marathi', short: 'MR' },
+  { code: 'or', label: 'Odia (Oriya)', short: 'OR' },
+  { code: 'pa', label: 'Punjabi', short: 'PA' },
+  { code: 'sa', label: 'Sanskrit', short: 'SA' },
+  { code: 'sat', label: 'Santali', short: 'SAT' },
+  { code: 'ta', label: 'Tamil', short: 'TA' },
+  { code: 'te', label: 'Telugu', short: 'TE' },
+  { code: 'ur', label: 'Urdu', short: 'UR' },
+  { code: 'awa', label: 'Awadhi', short: 'AWA' },
+  { code: 'mwr', label: 'Marwari (Marwadi)', short: 'MWR' },
+  { code: 'tcy', label: 'Tulu', short: 'TCY' },
+  { code: 'lus', label: 'Mizo', short: 'LUS' }
 ]
-// const languages = [
-//   { code: 'en', label: 'English', short: 'EN' },
-//   { code: 'as', label: 'Assamese', short: 'AS' },
-//   { code: 'bn', label: 'Bengali', short: 'BN' },
-//   { code: 'bho', label: 'Bhojpuri', short: 'BHO' },
-//   { code: 'doi', label: 'Dogri', short: 'DOI' },
-//   { code: 'gu', label: 'Gujarati', short: 'GU' },
-//   { code: 'hi', label: 'Hindi', short: 'HI' },
-//   { code: 'kn', label: 'Kannada', short: 'KN' },
-//   { code: 'kok', label: 'Konkani', short: 'KOK' },
-//   { code: 'mai', label: 'Maithili', short: 'MAI' },
-//   { code: 'ml', label: 'Malayalam', short: 'ML' },
-//   { code: 'mr', label: 'Marathi', short: 'MR' },
-//   { code: 'mni', label: 'Meiteilon (Manipuri)', short: 'MNI' },
-//   { code: 'or', label: 'Odia (Oriya)', short: 'OR' },
-//   { code: 'pa', label: 'Punjabi (Gurmukhi)', short: 'PA' },
-//   { code: 'sa', label: 'Sanskrit', short: 'SA' },
-//   { code: 'sd', label: 'Sindhi', short: 'SD' },
-//   { code: 'ta', label: 'Tamil', short: 'TA' },
-//   { code: 'te', label: 'Telugu', short: 'TE' },
-//   { code: 'ur', label: 'Urdu', short: 'UR' }
-// ];
 
 function LanguageSelector() {
   const [lang, setLang] = useState(localStorage.getItem('lang') || 'en')
@@ -64,14 +65,14 @@ function LanguageSelector() {
     const cookieValue = newLang === 'en' ? '/en/en' : '/en/' + newLang
 
     // Clear old googtrans cookies for specific domain and subdomain
-    document.cookie = `googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=frontend.72.60.83.68.nip.io`;
-    document.cookie = `googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.nip.io`;
-    document.cookie = `googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+    document.cookie = `googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=frontend.72.60.83.68.nip.io`
+    document.cookie = `googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.nip.io`
+    document.cookie = `googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`
 
     // Set new googtrans cookies for specific domain and subdomain
-    document.cookie = `googtrans=${cookieValue}; path=/; domain=frontend.72.60.83.68.nip.io`;
-    document.cookie = `googtrans=${cookieValue}; path=/; domain=.nip.io`;
-    document.cookie = `googtrans=${cookieValue}; path=/`;
+    document.cookie = `googtrans=${cookieValue}; path=/; domain=frontend.72.60.83.68.nip.io`
+    document.cookie = `googtrans=${cookieValue}; path=/; domain=.nip.io`
+    document.cookie = `googtrans=${cookieValue}; path=/`
 
     // Only reload if language is changing
     window.location.href = window.location.href.split('#')[0]
@@ -83,7 +84,7 @@ function LanguageSelector() {
         <select
           value={lang}
           onChange={(e) => changeLanguage(e.target.value)}
-          className="border border-[#0B1E2A] text-[rgba(11,30,42,0.9)] font-medium tracking-normal rounded-full px-4 lg:px-2 lg:pr-5 xl:pr-5 xl:px-3 py-0.5 lg:py-1  focus:outline-none focus:ring-2 focus:ring-blue-400 font-redhat appearance-none text-sm lg:text-xs"
+          className="border border-[#0B1E2A] text-[rgba(11,30,42,0.9)] font-medium tracking-normal w-20 rounded-full px-4 lg:px-2 lg:pr-5 xl:pr-5 xl:px-3 py-0.5 lg:py-1  focus:outline-none focus:ring-2 focus:ring-blue-400 font-redhat appearance-none text-sm lg:text-xs"
         >
           {languages.map((language) => (
             <option key={language.code} value={language.code}>
