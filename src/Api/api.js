dd5e8be27eb68c0c827a844c4bf08a971a1868ac
mynkbhurani <154728413+mynkbhurani@users.noreply.github.com>
2025-10-15 18:25:28 +0530

    Update api.js

--- src/Api/api.js
+++ src/Api/api.js
@@ -10,7 +10,7 @@ const api = axios.create({
 api.interceptors.request.use(config => {
   const user = getProviderUser();
 
-  if (user.access_token) {
+  if (user && user.access_token) {
     config.headers.Authorization = `Bearer ${user.access_token}`;
   }
 
