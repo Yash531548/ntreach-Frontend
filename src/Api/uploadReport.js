7147f88d019e17f6b1bd4cadff472e4b5a0b3cbd
mynkbhurani <154728413+mynkbhurani@users.noreply.github.com>
2025-12-22 17:31:54 +0530

    Update uploadReport.js

--- src/Api/uploadReport.js
+++ src/Api/uploadReport.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 const BASE_URL = import.meta.env.VITE_API_URL
 
-export const uploadReport = async (file, bookingId) => {
+export const uploadReport = async (files, bookingId) => {
   const token = localStorage.getItem('userToken')
 
   const formData = new FormData()
-  formData.append('report_file', file)
-  formData.append('booking_id', bookingId)
+  formData.append('evidence_files', files)
+  formData.append('appointment_id', bookingId)
 
   const response = await axios.post(`${BASE_URL}api/user/upload-report`, formData, {
     headers: {
