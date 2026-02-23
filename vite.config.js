0ea52d92af26a89181d90f3305a9f7765ac73762
mynkbhurani <154728413+mynkbhurani@users.noreply.github.com>
2026-02-23 13:53:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-		proxy: {
-			'/api': {
-				target: 'https://backend.72.60.83.68.nip.io',
-				changeOrigin: true,
-			},
-			'/storage': {
-				target: 'https://backend.72.60.83.68.nip.io',
-				changeOrigin: true,
-			},
-		},
-	},
-});
+  plugins: [react(), tailwindcss()]
+})
